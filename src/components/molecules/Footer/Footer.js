c35a00695f86a5425220864f36706c37eb9c432a
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2021-03-15 20:24:40 +0100

    Created default footer

--- src/components/molecules/Footer/Footer.js
+++ src/components/molecules/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footes.scss';
 
 const Footer = () => (
   <div className="container">
     <div className="wrapper">
-      <h1> 3333</h1>
+      <div className="column">
+        <h4>Categories</h4>
+        <ul className="footer-list">
+          <li>
+            <Link to="./">
+              <p> Women</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="./">
+              <p>Men</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="./">
+              <p>Shoes</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="./">
+              <p> Watches</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 );
