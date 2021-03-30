8cff402c257e4c6dd1752587f3786e090981cc19
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2021-03-30 21:35:34 +0200

    add image in modal

--- src/components/organism/ShopProducts/Modal/Modal.js
+++ src/components/organism/ShopProducts/Modal/Modal.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './Modal.scss';
 import PropTypes from 'prop-types';
+import imgModal1 from '../../../../assets/images/products/product-detail-01.png';
+import imgModal2 from '../../../../assets/images/products/product-detail-02.png';
+import imgModal3 from '../../../../assets/images/products/product-detail-03.png';
 
 const Modal = ({ showModal, setShowModal }) => (
   <>
     {showModal ? (
       <div className="modal" showModal={showModal}>
+        <button type="button" className="close-modal" onClick={() => setShowModal(prev => !prev)}>
+          Close
+        </button>
         <div className="wrapper-modal">
-          <h1>gggggg</h1>
-          <button type="button" className="close-modal" onClick={() => setShowModal(prev => !prev)}>
-            Close
-          </button>
+          <div className=" section-image">
+            <ul className="small-sectionImg">
+              <li className="presentation-img">
+                <img src={imgModal1} alt="imgModal" className="modal-img" />
+              </li>
+              <li className="presentation-img">
+                <img src={imgModal2} alt="imgModal" className="modal-img" />
+              </li>
+              <li className="presentation-img">
+                <img src={imgModal3} alt="imgModal" className="modal-img" />
+              </li>
+            </ul>
+          </div>
+          <div className="section-add">
+            <h1>ahahshhshsh</h1>
+          </div>
         </div>
       </div>
     ) : null}
