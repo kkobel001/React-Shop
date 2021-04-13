6ad1d3895063b073207aa0a757fe095ec9480d07
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2021-04-13 21:09:36 +0200

    Refactoring modal

--- src/components/organism/ShopProducts/AddProducts/AddQuality.js
+++ src/components/organism/ShopProducts/AddProducts/AddQuality.js
@@ -6,32 +6,33 @@ import './AddQuality.scss';
 export default class AddQuality extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { qty: 0 };
+    this.state = { qty: 1 };
 
     this.handleClick = this.handleClick.bind(this);
     this.removeQty = this.removeQty.bind(this);
   }
 
   handleClick() {
-    this.setState(state => ({ qty: state.qty + 1 }));
+    this.setState(prevState => ({ qty: ++prevState.qty }));
   }
 
   removeQty() {
-    this.setState(state => ({ qty: state.qty - 1 }));
+    this.setState(prevState => ({ qty: prevState.qty > 0 ? --prevState.qty : 0 }));
   }
 
   render() {
     const { qty } = this.state;
     return (
       <div className="products-number">
-        <button type="button" className="icon-number" onClick={this.handleClick}>
-          <AddIcon className="icon-design" />
+        <button className="icon-number" type="button" onClick={this.removeQty}>
+          <RemoveIcon className="icon-design" />
         </button>
+
         <div className="number-box" min="1" max="10">
           {qty}
         </div>
-        <button className="icon-number" type="button" onClick={this.removeQty}>
-          <RemoveIcon className="icon-design" />
+        <button type="button" className="icon-number" onClick={this.handleClick}>
+          <AddIcon className="icon-design" />
         </button>
       </div>
     );
