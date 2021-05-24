a4e67d2686b8b885fa0e88be63abf9785a824da0
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2021-05-24 22:00:59 +0200

    Filx a problem with url image

--- src/components/molecules/DetailsItemBlog/DetailsItemBlog.js
+++ src/components/molecules/DetailsItemBlog/DetailsItemBlog.js
@@ -1,41 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-//
-
-// const dataDetailsItem = [
-//   {
-//     id: 1,
-//     title: 'test',
-//     image: 'https://www.thoughtco.com/thmb/C7RiS4QG5TXcBG2d_Sh9i4hFpg0=/3620x2036/smart/filters:no_upscale()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg',
-//     context:
-//       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose',
-//   },
-//   {
-//     id: 2,
-//     title: 'test',
-//     image: 'https://www.thoughtco.com/thmb/C7RiS4QG5TXcBG2d_Sh9i4hFpg0=/3620x2036/smart/filters:no_upscale()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg',
-//     context:
-//       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose',
-//   },
-//   {
-//     id: 3,
-//     title: 'test',
-//     image: 'https://www.thoughtco.com/thmb/C7RiS4QG5TXcBG2d_Sh9i4hFpg0=/3620x2036/smart/filters:no_upscale()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg',
-//     context:
-//       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose',
-//   },
-// ];
 
 function DetailsItemBlog() {
-  const [detailsArticle, setDetailsArticle] = useState({});
+  const [detailsArticle, setDetailsArticle] = useState(false);
   const [error, setError] = useState('');
   const { articleId } = useParams();
 
   useEffect(() => {
     axios
       .post(
-        ' https://graphql.datocms.com/',
+        'https://graphql.datocms.com/',
         {
           query: `
           {
@@ -43,6 +18,7 @@ function DetailsItemBlog() {
                 id
                 title
                 context
+                contend
                 image
                 {
                   url
@@ -57,15 +33,17 @@ function DetailsItemBlog() {
         },
       )
       .then(({ data: { data } }) => {
-        setDetailsArticle(data.allArticles[0]);
+        if (data.allArticles.length > 0) {
+          setDetailsArticle(data.allArticles[0]);
+        }
       })
       .catch(() => setError("Sorry, we couln't load articles for you"));
   }, []);
-  // console.log(detailsArticle);
+
   return detailsArticle ? (
     <div className="wrapper-blogItems">
       <h2>{detailsArticle.title}</h2>
-      {/* <img src={detailsArticle.image.url} alt="ggg" /> */}
+      <img src={detailsArticle.image.url} alt="blog-img" />
       <div>{detailsArticle.context}</div>
     </div>
   ) : (
