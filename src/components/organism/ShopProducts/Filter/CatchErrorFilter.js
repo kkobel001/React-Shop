a67f17896df6b6601ff32d978f1f075c06e63e75
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2021-12-07 23:11:48 +0100

    Refactoring filter

---
+++ src/components/organism/ShopProducts/Filter/CatchErrorFilter.js
@@ -0,0 +1,11 @@
+function CatchErrorFilter() {
+  try {
+    console.log('hhhhh');
+  } catch (error) {
+    console.log('hhhhh');
+  } finally {
+    console.log('hhhhh');
+  }
+}
+
+export default CatchErrorFilter;
