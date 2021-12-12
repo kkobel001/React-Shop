66042ed0cba9dea51db3ed7b8aeb65422fa0fc52
Anna Mierzwinska <amierzwinska@cybervadis.com>
2021-12-12 22:15:59 +0100

    update useSetData

--- src/hooks/useSetData.js
+++ src/hooks/useSetData.js
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 import { getDatabase, ref, set } from 'firebase/database';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const useSetData = () => {
   const [err, setError] = useState(null);
   const [isSaving, setIsSaving] = useState(false);
 
   const setData = async (path, data) => {
-    setIsSaving(true);
-    try {
-      await set(ref(getDatabase(), path), data);
-      console.log('Data saved successfully!');
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsSaving(false);
-    }
+    onAuthStateChanged(getAuth(), async () => {
+      setIsSaving(true);
+      try {
+        await set(ref(getDatabase(), path), data);
+        console.log('Data saved successfully!');
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsSaving(false);
+      }
+    });
   };
 
   return { setData, err, isSaving };
