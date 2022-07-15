b9b360d3750237a487a0b66744f1aee86355653a
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-07-15 23:12:13 +0200

    Add tests in Filter

--- src/components/organism/ContactForm/ContactForm.test.js
+++ src/components/organism/ContactForm/ContactForm.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 describe('correct render in components', () => {
+  // const onSubmit = jest.fn();
+
   it('Render the component', () => {
     render(<ContactForm />);
     fireEvent.change(screen.getByPlaceholderText('Name'), { target: { value: 'Kasia' } });
     fireEvent.change(screen.getByPlaceholderText('Your Email Address'), { target: { value: 'kasia.kobel@interia.pl' } });
     fireEvent.change(screen.getByPlaceholderText('Name'), { target: { value: 'How Can We Help?' } });
     fireEvent.click(screen.getByText('Send'));
+  });
+
+  it('has 3 required fields on first step', async () => {
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText('Name'), { target: { value: 'Kasia' } }).toHaveErrorMessage('Name is required');
+    });
+
+    // expect(getCity()).toHaveErrorMessage('city is a required field');
+    // expect(getSelectJobSituation()).toHaveErrorMessage(
+    //   'You need to select your job situation'
+    // );
   });
 });
