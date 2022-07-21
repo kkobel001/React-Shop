2d57eaec75795f92e051c3db6105b8f87b591d42
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-07-21 22:46:48 +0200

    Fix button on Filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from 'services/firebase';
 import { Provider } from 'react-redux';
@@ -12,12 +12,11 @@ initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Root />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  // <React.StrictMode>
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  // </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
